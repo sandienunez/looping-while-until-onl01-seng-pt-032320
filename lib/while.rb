687cd58ede103c2e55dec5ab74fb4ae046a0c25c
Sandie Nunez <snunez@vt.edu>
2020-03-30 16:46:03 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,18 @@
+#write a while loop
+#continue to puts the phrase "Wingardium Leviosa" 
+#while our levitation force is less than 10. 
+#Every time we puts that phrase, we should increment our levitation force by 1
+
 def using_while
   levitation_force = 6
 	
-  #your code here
+  while levitation_force < 10
+    binding.pry
+    puts "Wingardium Leviosa"
+    #levitation_force = levitation_force + 1 
+    levitation_force += 1 
+  end
+  
 end
 
 
